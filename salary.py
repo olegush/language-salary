026a9c3404cf4d39b38a23236a34864aec432345
olegush <43958767+olegush@users.noreply.github.com>
2018-12-16 12:54:17 +0300

    solved decomposition problems

--- salary.py
+++ salary.py
@@ -40,55 +40,99 @@ def get_lang_rankings(filepath):
         return json.loads(file.read())
 
 
+def get_predict_salary(salary_from, salary_to):
+    if salary_from and salary_to:
+        salary = int(salary_from + salary_to / 2)
+    elif not salary_to:
+        salary = int(salary_from * 1.2)
+    else:
+        salary = int(salary_to * 0.8)
+    return salary
+
+
 def get_predict_rub_salary_hh(vacancy):
-    if vacancy['salary'] and vacancy['salary']['currency'] in 'RUR':
-        if vacancy['salary']['from'] and vacancy['salary']['to']:
-            return int(vacancy['salary']['from'] + vacancy['salary']['to'] / 2)
-        elif not vacancy['salary']['to']:
-            return int(vacancy['salary']['from'] * 1.2)
-        else:
-            return int(vacancy['salary']['to'] * 0.8)
+    salary = vacancy['salary']
+    if salary and salary['currency'] in 'RUR':
+        return get_predict_salary(
+            salary['from'],
+            salary['to']
+        )
 
 
 def get_predict_rub_salary_sj(vacancy):
     if vacancy['currency'] in 'rub':
-        if vacancy['payment_from'] and vacancy['payment_to']:
-            return int(vacancy['payment_from'] + vacancy['payment_to'] / 2)
-        elif not vacancy['payment_to']:
-            return int(vacancy['payment_from'] * 1.2)
-        elif not vacancy['payment_from']:
-            return int(vacancy['payment_to'] * 0.8)
+        return get_predict_salary(
+            vacancy['payment_from'],
+            vacancy['payment_to']
+        )
 
 
-def get_salary_by_langs_hh(languages, top, region, period):
+def get_salaries_hh(language, region, period):
     hh_api_url = 'https://api.hh.ru/vacancies'
     headers = {}
+    pages = 1
+    page = 1
+    salaries = []
+    founded = 0
+    while page <= pages:
+        # HH.RU API parameters https://github.com/hhru/api/
+        payload = {
+            'specialization': '1.221',
+            'area': region,
+            'period': period,
+            'text': language['lang'],
+            'page': page
+        }
+        vacancies = get_vacancies(hh_api_url, headers, payload)
+        salaries += [
+            get_predict_rub_salary_hh(vacancy)
+            for vacancy in vacancies['items']
+            if get_predict_rub_salary_hh(vacancy)
+        ]
+        time.sleep(random.random() * 3)
+        page += 1
+        pages = vacancies['pages']
+        founded = vacancies['found']
+    return salaries, founded
+
+
+def get_salaries_sj(language, region, period):
+    load_dotenv()
+    secret_key = os.getenv('SECRET_KEY')
+    superjob_api_url = 'https://api.superjob.ru/2.0/vacancies/'
+    headers = {'X-Api-App-Id': secret_key}
+    more = True
+    page = 0
+    salaries = []
+    founded = 0
+    while more:
+        # SuperJob API parameters
+        # https://api.superjob.ru/#search_vacanices
+        payload = {
+            't': region,
+            'catalogues': '48',
+            'count': '20',
+            'page': page,
+            'keyword': language['lang'],
+            'period': period
+        }
+        vacancies = get_vacancies(superjob_api_url, headers, payload)
+        salaries += [
+            get_predict_rub_salary_sj(vacancy)
+            for vacancy in vacancies['objects']
+            if get_predict_rub_salary_sj(vacancy)
+        ]
+        time.sleep(random.random() * 3)
+        page += 1
+        more = vacancies['more']
+        founded = vacancies['total']
+    return salaries, founded
+
+
+def get_avg_salary_hh(languages, top, region, period):
     salary_by_langs = {}
     for language in languages[:top]:
-        pages = 1
-        page = 1
-        salaries = []
-        founded = 0
-        while page <= pages:
-            # HH.RU API parameters https://github.com/hhru/api/
-            payload = {
-                'specialization': '1.221',
-                'area': region,
-                'period': period,
-                'text': language['lang'],
-                'page': page
-            }
-            vacancies = get_vacancies(hh_api_url, headers, payload)
-            salaries += [
-                get_predict_rub_salary_hh(vacancy)
-                for vacancy in vacancies['items']
-                if get_predict_rub_salary_hh(vacancy)
-            ]
-            sec = random.random() * 3
-            time.sleep(sec)
-            page += 1
-            pages = vacancies['pages']
-            founded = vacancies['found']
+        salaries, founded = get_salaries_hh(language, region, period)
         avg_salary = '{:,}'.format(int(sum(salaries) / len(salaries)))
         salary_by_langs[language['lang']] = dict(
             rank=language['rank'],
@@ -99,39 +143,10 @@ def get_salary_by_langs_hh(languages, top, region, period):
     return salary_by_langs
 
 
-def get_salary_by_langs_sj(languages, top, region, period):
-    load_dotenv()
-    secret_key = os.getenv('SECRET_KEY')
-    superjob_api_url = 'https://api.superjob.ru/2.0/vacancies/'
-    headers = {'X-Api-App-Id': secret_key}
+def get_avg_salary_sj(languages, top, region, period):
     salary_by_langs = {}
     for language in languages[:top]:
-        more = True
-        page = 0
-        salaries = []
-        founded = 0
-        while more:
-            # SuperJob API parameters
-            # https://api.superjob.ru/#search_vacanices
-            payload = {
-                't': region,
-                'catalogues': '48',
-                'count': '20',
-                'page': page,
-                'keyword': language['lang'],
-                'period': period
-            }
-            vacancies = get_vacancies(superjob_api_url, headers, payload)
-            salaries += [
-                get_predict_rub_salary_sj(vacancy)
-                for vacancy in vacancies['objects']
-                if get_predict_rub_salary_sj(vacancy)
-            ]
-            sec = random.random() * 3
-            time.sleep(sec)
-            page += 1
-            more = vacancies['more']
-            founded = vacancies['total']
+        salaries, founded = get_salaries_sj(language, region, period)
         if len(salaries):
             avg_salary = '{:,}'.format(int(sum(salaries) / len(salaries)))
         else:
@@ -198,11 +213,11 @@ if __name__ == '__main__':
         'the public GitHub timeline (April 10 2018)\n'
         'For details visit https://github.com/benfred/github-analysis\n')
     print_table(
-        get_salary_by_langs_hh(lang_trends, top, region_hh_code, period),
+        get_avg_salary_hh(lang_trends, top, region_hh_code, period),
         'HeadHunter {}, {} days statistic'.format(region_name, period)
     )
     print()
     print_table(
-        get_salary_by_langs_sj(lang_trends, top, region_sj_code, period),
+        get_avg_salary_sj(lang_trends, top, region_sj_code, period),
         'SuperJob {}, {} days statistic'.format(region_name, period)
     )
